0cbafbff01c34155e59dcba39d8ec179169f539f
abert2 <aniyonsenga@gmail.com>
2021-11-06 11:14:23 -0400

    updated testing function

--- Examples/fake_turntable/fake_turntable.ino
+++ Examples/fake_turntable/fake_turntable.ino
@@ -2,10 +2,10 @@
 // Include the AccelStepper library:
 #include <AccelStepper.h>
 // Motor pin definitions:
-#define motorPin1  11      // IN1 on the ULN2003 driver
-#define motorPin2  10      // IN2 on the ULN2003 driver
-#define motorPin3  9     // IN3 on the ULN2003 driver
-#define motorPin4  8     // IN4 on the ULN2003 driver
+#define motorPin1  8      // IN1 on the ULN2003 driver
+#define motorPin2  9      // IN2 on the ULN2003 driver
+#define motorPin3  10     // IN3 on the ULN2003 driver
+#define motorPin4  11     // IN4 on the ULN2003 driver
 // Define the AccelStepper interface type; 4 wire motor in half step mode:
 #define MotorInterfaceType 4
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper library with 28BYJ-48 stepper motor:
@@ -20,6 +20,7 @@ int state = 0;
 
 //Set spinning speed
 int clockSpeed = 1000;
+float pos = 0; //current stepper position
 
 // use 2ms debounce time
 #define DEBOUNCE_TICKS (word)microsecondsToClockCycles(15)
@@ -49,11 +50,11 @@ void loop() {
     stepper.runSpeed();
   } 
   else if (state == 1) { //Motor follows pot
-    stepper.setSpeed(clockSpeed);
-    stepper.moveTo(200);
-    stepper.runSpeedToPosition();
-    stepper.moveTo(-200);
-    stepper.runSpeedToPosition();
+    stepper.setSpeed(10);
+    stepper.runSpeed();
+    pos = stepper.currentPosition();
+    Serial.print("Current Position: ");
+    Serial.println(pos);
   }
   else if (state == 2) { //Motor turns at constant speed
     stepper.setSpeed(clockSpeed);
